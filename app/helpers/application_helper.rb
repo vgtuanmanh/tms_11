95b39bc01dc16094fc59b2236a80fe064984b9f3
Manh Vuong Tuan <vgtuanmanh@gmail.com>
2015-01-28 09:09:01 +0700

    Add static pages and simple layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')                     
+    base_title = "Framgia Training Management System - tms_11"  
+    if page_title.empty?                              
+      base_title                                      
+    else
+      "#{page_title} | #{base_title}"                 
+    end
+  end
 end
