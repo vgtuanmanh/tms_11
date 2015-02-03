4ac732246440e426bfddb34cb0968f1ea3d8ed10
Manh Vuong Tuan <vgtuanmanh@gmail.com>
2015-02-03 15:03:35 +0700

    View and update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def show
@@ -18,9 +25,41 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in!"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    if !current_user?(@user)
+      store_location
+      flash[:danger] = "You do not have permission to edit
+                        the other user's profile!"
+      redirect_to users_path
+    end
   end
 end
