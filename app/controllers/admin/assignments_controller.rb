7beb19c1d84ded5f6ad38d6f654e630b1b5dc2a8
vgtuanmanh <vgtuanmanh@gmail.com>
2015-02-27 11:34:59 +0700

    Add admin to course

--- app/controllers/admin/assignments_controller.rb
+++ app/controllers/admin/assignments_controller.rb
@@ -1,12 +1,22 @@
 class Admin::AssignmentsController < ApplicationController
+  before_action :is_admin_user
   before_action :set_course
+
   def new
     @assignment = @course.assignments.new
-    @users = User.assignable(@course) - User.is_admin
+    @users = User.assignable(@course)
+  end
+
+  def index
+    @users = @course.users
   end
 
   private
   def set_course
     @course = Course.find params[:course_id]
+  end
+
+  def is_admin_user
+    redirect_to root_url unless admin_user?
   end
 end
